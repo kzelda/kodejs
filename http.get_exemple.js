7186d0828e524430072eae9c0c9b2482c6592161
kzelda <kzeldan64@gmail.com>
2013-02-26 15:40:28 +0100

    20130226

---
+++ http.get_exemple.js
@@ -0,0 +1,11 @@
+var vv;
+var http = require("http");
+http.get("http://www.google.com/index.html", function(res) {
+  console.log("Got response: " + res.statusCode);
+   res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
